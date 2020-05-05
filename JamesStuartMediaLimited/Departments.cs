8e8b6e8e0f381e242079d0970c108b0bb2e48589
abayomi igwubor <igwubor@outlook.com>
2020-05-05 09:11:58 -0700

    Add project files.

---
+++ JamesStuartMediaLimited/Departments.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JamesStuartMediaLimited
+{
+    class Departments : DictionaryBase
+    {
+        public void Add(string name, string dept)
+        {
+            Dictionary.Add(name, dept);
+        }
+
+        public void OnRemove(string name)
+        {
+            Console.WriteLine($"You are going to delete record containing ID : {name}");
+            Dictionary.Remove(name);
+        }
+
+        public void GetDetails()
+        {
+            IDictionaryEnumerator objEnumerate =
+            Dictionary.GetEnumerator();
+            while (objEnumerate.MoveNext())
+            {
+                Console.WriteLine(objEnumerate.Key.ToString() + "\t\t" + objEnumerate.Value);
+            }
+        }
+    }
+}
---
+++ JamesStuartMediaLimited/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace JamesStuartMediaLimited
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Departments departments = new Departments();
+           // Dictionary<string, string> departments = new Dictionary<string, string>();
+            departments.Add("Tom Lee", "Training");
+            departments.Add("James Anderson", "HR");
+            departments.Add("Micheal Chan", "Content Development");
+            departments.Add("Jakie Jackson", "Training");
+            departments.Add("Bruce Douglas", "Graphics");
+            departments.Add("Kim Date", "HR");
+            departments.Add("Joseph Mathews", "Content Development");
+            departments.Add("Rose Dorson", "Marketing");
+            departments.Add("Andrews Stephan", "Graphics");
+
+
+
+            departments.GetDetails();
+
+
+            SortedList depts = new SortedList();
+            depts.Add("Tom Lee", "Training"); depts.Add("James Anderson", "HR"); depts.Add("Micheal Chan", "Content Development"); depts.Add("Jakie Jackson", "Training"); depts.Add("Bruce Douglas", "Graphics"); depts.Add("Kim Date", "HR"); depts.Add("Joseph Mathews", "Content Development"); depts.Add("Rose Dorson", "Marketing"); depts.Add("Andrews Stephan", "Graphics");
+
+            
+            Console.WriteLine(depts.Contains("Andrews Stephan"));
+
+            depts.Remove("Andrews Stephan");
+
+            Console.WriteLine(depts.Contains("Andrews Stephan"));
+
+
+            foreach (DictionaryEntry dept in depts)
+            {
+                Console.WriteLine($"{dept.Key} {dept.Value}");
+            }
+
+
+
+            // foreach (string key in departments.Keys)
+            // {
+            //   Console.WriteLine(key+ ":" + departments[key]);
+            // }
+        }
+    }
+}
